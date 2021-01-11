8bd05e47820845847addeaf00812baaaeb48c749
github-action <github-action@github.com>
2021-01-11 15:11:47 +0000

    Automatic deploy

--- src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const greet = require('./greet');
-
-module.exports = {
-  greet
-};
--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { greet } = require('../src/index');
-
-describe('Test index', () => {
-  test('should greet to be truthy', () => {
-    expect(greet).toBeTruthy();
-  });
-});
