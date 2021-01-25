da5a2d32e175c460284abb8ab240f56c20bb45e6
rdarida <robertdarida86@gmail.com>
2021-01-25 12:36:38 +0100

    chore: update package.json

--- src/greet.js
+++ src/greet.js
@@ -3,7 +3,7 @@
 /**
  * Gets a name and returns a greeting with it.
  * @param {string=} name Name of somebody to greet.
- * @return {string} The greeting.
+ * @returns {string} The greeting.
  */
 function greet(name = 'World') {
   return `Hello, ${name.trim()}!`;
